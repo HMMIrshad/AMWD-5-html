58fb0068f01adad359bcdc9a822e8be5962770bb
HMMIrshad <irs122333@gmail.com>
2024-06-23 10:47:56 +0530

    first commit

---
+++ Jquery/index.js
@@ -0,0 +1,11 @@
+// $('#show-btn').click(function(){
+//     $('.view-1').show();
+// })
+
+// $('#hide-btn').dblclick(function(){
+//     $('.view-1').hide();
+// })
+
+// $(document).ready(function(){
+//     $('.view-1').hide();
+// })
